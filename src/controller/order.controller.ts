650538a029c324631743b13315462cb0c7120b39
David Armendariz <david.adrian.a.p@gmail.com>
2024-10-12 10:23:01 -0500

    Adds kafka

--- src/controller/order.controller.ts
+++ src/controller/order.controller.ts
@@ -8,6 +8,7 @@ import Stripe from "stripe";
 import { client } from "../index";
 import { User } from "../entity/user.entity";
 import { createTransport } from "nodemailer";
+import { kafkaProducer } from "../kafka/config";
 
 export const Orders = async (req: Request, res: Response) => {
   const orders = await getRepository(Order).find({
@@ -23,7 +24,7 @@ export const Orders = async (req: Request, res: Response) => {
       total: order.total,
       created_at: order.created_at,
       order_items: order.order_items,
-    })),
+    }))
   );
 };
 
@@ -138,21 +139,14 @@ export const ConfirmOrder = async (req: Request, res: Response) => {
     port: 1025,
   });
 
-  await transporter.sendMail({
-    from: "from@example.com",
-    to: "admin@admin.com",
-    subject: "An order has been completed",
-    html: `Order #${order.id} with a total of $${order.total} has been completed`,
+  const value = JSON.stringify({
+    ...order,
+    admin_revenue: order.total,
+    ambassador_revenue: order.ambassador_revenue,
   });
 
-  await transporter.sendMail({
-    from: "from@example.com",
-    to: order.ambassador_email,
-    subject: "An order has been completed",
-    html: `You earned $${order.ambassador_revenue} from the link #${order.code}`,
-  });
-
-  await transporter.close();
+  await kafkaProducer.connect();
+  await kafkaProducer.send({ topic: "default", messages: [{ value }] });
 
   res.send({
     message: "success",
---
+++ src/kafka/config.ts
@@ -0,0 +1,17 @@
+import { Kafka } from "kafkajs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const kafka = new Kafka({
+  clientId: "email-producer",
+  brokers: [process.env.KAFKA_BROKER],
+  ssl: true,
+  sasl: {
+    mechanism: "plain",
+    username: process.env.KAFKA_API_KEY,
+    password: process.env.KAFKA_SECRET_KEY,
+  },
+});
+
+export const kafkaProducer = kafka.producer();
