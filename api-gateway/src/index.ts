85829c6a239ecf031b379d4d472a3ef76e3698e7
dalopeza98 <david.lopeza98@gmail.com>
2024-10-19 18:07:53 -0500

    Added API Gateway for Ambassador APIs

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -4,6 +4,7 @@ import { routes } from "./routes";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import { debug } from "console";
 
 dotenv.config();
 
@@ -26,25 +27,35 @@ app.use(
 
 // Configuración del API Gateway para redirigir todas las peticiones de ambassador
 const ambassadorServiceProxyAmbassador = createProxyMiddleware({
-  target: `http://localhost:${process.env.PORT_AMBASSADOR}/api/ambassador`, // Cambia esta URL por el servidor que maneja las rutas de ambassador
+  target: `http://localhost:3701`,
   changeOrigin: true,
 });
 
 const ambassadorServiceProxyCheckout = createProxyMiddleware({
-  target: `http://localhost:${process.env.PORT_AMBASSADOR}/api/checkout`, // Cambia esta URL por el servidor que maneja las rutas de ambassador
+  target: `http://localhost:3701`, // No incluyas /api/checkout en el target
   changeOrigin: true,
 });
 
 const ambassadorServiceProxyAdmin = createProxyMiddleware({
-  target: `http://localhost:${process.env.PORT_AMBASSADOR}/api/admin`, // Cambia esta URL por el servidor que maneja las rutas de ambassador
+  target: `http://localhost:3701`,
   changeOrigin: true,
+  pathRewrite: {
+    "^/api/admin": "/api/admin", // Mantén el prefijo /api/admin
+  },
 });
 
 // Aplicar el proxy para todas las rutas de ambassador
-app.use("/api/ambassador", ambassadorServiceProxyAmbassador);
-app.use("/api/checkout", ambassadorServiceProxyCheckout);
-app.use("/api/admin", ambassadorServiceProxyAdmin);
+module.exports = function (app) {
+  app.use("/api/checkout", ambassadorServiceProxyCheckout);
+};
 
+module.exports = function (app) {
+  app.use("/api/ambassador", ambassadorServiceProxyAmbassador);
+};
+
+module.exports = function (app) {
+  app.use("/api/admin", ambassadorServiceProxyAdmin);
+};
 // Mantener las demás rutas
 routes(app);
 
