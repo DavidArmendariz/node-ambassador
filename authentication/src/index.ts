1c2d680e93daee4259d0e51b9c987457dde261e1
David Armendariz <david.adrian.a.p@gmail.com>
2024-10-19 12:57:42 -0500

    WIP on Kafka

--- authentication/src/index.ts
+++ authentication/src/index.ts
@@ -2,18 +2,25 @@ import express from "express";
 import { routes } from "./routes";
 import dotenv from "dotenv";
 import { AppDataSource } from "../data-source";
+import { kafkaConsumer } from "./kafka/config";
 
 dotenv.config();
 
 const PORT = 8001;
 
-AppDataSource.initialize().then(async () => {
-  const app = express();
-  app.use(express.json());
+AppDataSource.initialize()
+  .then(async () => {
+    await kafkaConsumer.connect();
+    await kafkaConsumer.subscribe({ topic: "authentication" });
+    const app = express();
+    app.use(express.json());
 
-  routes(app);
+    routes(app);
 
-  app.listen(PORT, () => {
-    console.log(`listening to port ${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`listening to port ${PORT}`);
+    });
+  })
+  .finally(async () => {
+    await kafkaConsumer.disconnect();
   });
-});
