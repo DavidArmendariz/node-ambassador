b42700b863ad59f458c56be20553f10ffc1d924f
David Armendariz <david.adrian.a.p@gmail.com>
2024-10-19 13:29:26 -0500

    Fixes initialization of typeorm

--- users/src/index.ts
+++ users/src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
-import { createConnection } from "typeorm";
 import { routes } from "./routes";
 import dotenv from "dotenv";
 import { kafkaProducer } from "./kafka/config";
+import { AppDataSource } from "../data-source";
 
 dotenv.config();
 
-const PORT = 8001;
+const PORT = 3700;
 
-createConnection().then(async () => {
+AppDataSource.initialize().then(async () => {
   await kafkaProducer.connect();
   const app = express();
   app.use(express.json());
