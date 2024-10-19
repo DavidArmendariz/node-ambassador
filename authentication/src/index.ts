890b0b3073123347b53ba420e6be9bdcdd0a96da
David Armendariz <david.adrian.a.p@gmail.com>
2024-10-19 12:50:22 -0500

    Removes create connection

--- authentication/src/index.ts
+++ authentication/src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { createConnection } from "typeorm";
 import { routes } from "./routes";
 import dotenv from "dotenv";
 import { AppDataSource } from "../data-source";
